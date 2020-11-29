0ec9c4a1f3de3042ad1e334c55880ce1d48cbaf2
Pavel Kosilo <kodosgames@gmail.com>
2020-11-29 21:37:10 +0300

    Update tests (remove everything)

--- Tests/GameNetworkTests/GameNetworkTests.swift
+++ Tests/GameNetworkTests/GameNetworkTests.swift
@@ -6,7 +6,7 @@ final class GameNetworkTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(GameNetwork().text, "Hello, World!")
+        // XCTAssertEqual(GameNetwork().text, "Hello, World!")
     }
 
     static var allTests = [
